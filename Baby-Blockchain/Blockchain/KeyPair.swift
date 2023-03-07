73f12883446e321c75bf9badcf7ad71eab829379
Igor Zhukov <ihorzhukov@gmail.com>
2023-03-07 16:27:25 +0000

    add ECC SignatureService and KeyPair (#1)
    *add ECC SignatureService and KeyPair

---
+++ Baby-Blockchain/Blockchain/KeyPair.swift
@@ -0,0 +1,13 @@
+//
+//  KeyPair.swift
+//  Baby-Blockchain
+//
+//  Created by Igor Zhukov on 14.02.2023.
+//
+
+import Foundation
+
+struct KeyPair {
+    let privateKey: SecKey
+    let publicKey: SecKey
+}
--- Baby-Blockchain/ViewController.swift
+++ Baby-Blockchain/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let message = "Text to sign!"
+        let signatureService = SignatureService()
+        
+        do {
+            let keyPair = try signatureService.createECCKeyPair()
+            
+            let signature = try signatureService.sign(textToSign: message,
+                                                      privateKey: keyPair.privateKey)
+            
+            let signatureIsVerified = try signatureService.verifySignature(textToSign: message,
+                                                                           signature: signature,
+                                                                           publicKey: keyPair.publicKey)
+            print(signatureIsVerified)
+        } catch let error {
+            print(error.localizedDescription)
+        }
     }
-
-
 }
-
